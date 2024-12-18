2fdc31eca81cdd91a8bdc52f484072023e450687
VMTSTUDIO <tvthuan516@gmail.com>
2024-12-18 19:45:01 +0700

    Add files via upload

---
+++ lua.lua
@@ -0,0 +1,6 @@
+Config = {
+    namescript = "Thuận Hub",--Tên Script
+    logoscript = "117390141608083",--ID Logo Script
+    tacgia = "Thuận Vũ"-- Tên Tác Giả
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
